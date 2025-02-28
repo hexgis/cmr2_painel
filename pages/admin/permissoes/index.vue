18cffc11e461fc3527db7e8cce263b09f5ec5e29
Joao Fonseca <joao.fonseca@hex360.com.br>
2025-02-28 11:55:02 -0300

    feat (admin) hidden unused menus

--- pages/admin/permissoes/index.vue
+++ pages/admin/permissoes/index.vue
@@ -1,38 +1,45 @@
 <template>
-    <div class="permissions">
-        <h1 class="text-uppercase pb-4">Gerenciar Permissões</h1>
-        <FullCard v-for="label in props" :key="label.url" :props="label" />
-    </div>
+  <div class="permissions">
+    <h1 class="text-uppercase pb-4">
+      Gerenciar Permissões
+    </h1>
+    <FullCard
+      v-for="label in props"
+      :key="label.url"
+      :props="label"
+    />
+  </div>
 </template>
 
 <script>
-import FullCard from '/components/admin/FullCard.vue'
+import FullCard from '/components/admin/FullCard.vue';
+
 export default {
-    components: { FullCard },
-    name: 'GerenciarPermissoes',
-    layout: 'admin',
-    data() {
-        return {
-            props: [
-                {
-                    url: 'permissoes/grupos/',
-                    icon: 'mdi-group',
-                    optionLabel: 'Grupos',
-                },
-                {
-                    url: 'permissoes/instituicoes/',
-                    icon: 'mdi-office-building',
-                    optionLabel: 'Instituições',
-                },
-                {
-                    url: 'permissoes/fpe/',
-                    icon: 'mdi-tree',
-                    optionLabel: 'FPE - Frente de Proteção Etnoambiental',
-                },
-            ],
-        }
-    },
-}
+  name: 'GerenciarPermissoes',
+  components: { FullCard },
+  layout: 'admin',
+  data() {
+    return {
+      props: [
+        {
+          url: 'permissoes/grupos/',
+          icon: 'mdi-group',
+          optionLabel: 'Grupos',
+        },
+        {
+          url: 'permissoes/instituicoes/',
+          icon: 'mdi-office-building',
+          optionLabel: 'Instituições',
+        },
+        // {
+        //   url: 'permissoes/fpe/',
+        //   icon: 'mdi-tree',
+        //   optionLabel: 'FPE - Frente de Proteção Etnoambiental',
+        // },
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="sass" scoped>
