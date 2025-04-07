32a1e64c48d4b7008895fad37ecf08ebedde0252
Victor Ferreira <victor.ferreira@hex360.com.br>
2025-04-07 12:09:58 -0300

    Feat: change landing page to portal

--- pages/index.vue
+++ pages/index.vue
@@ -1,328 +1,183 @@
 <template>
-  <v-tabs
-    v-model="activeTab"
-    vertical
-    optional
-    background-color="primary"
-    class="right-tabs fill-height"
-  >
-    <v-tab
-      v-for="(tab, i) in tabs"
-      :key="i"
-      :to="localePath(tab.route)"
-      nuxt
-      :disabled="showTableDialog"
-    >
-      <v-tooltip left>
-        <template #activator="{ on }">
-          <v-icon
-            class="tab-icon"
-            v-on="on"
-          >
-            {{ tab.icon }}
-          </v-icon>
-        </template>
-        <span>{{ tab.name }}</span>
-      </v-tooltip>
-    </v-tab>
-
-    <v-tab-item
-      :transition="false"
-      :reverse-transition="false"
-    >
-      <div
-        id="img-wrapper"
-        class="d-flex justify-center"
-      >
-        <v-img
-          contain
-          :src="logo_funai"
-        />
-      </div>
-      <nuxt-child keep-alive />
-
-      <div
-        v-if="isIndex"
-        class="info d-flex flex-column align-content-space-between"
-        style="height: 90%;"
-      >
-        <v-list>
-          <v-list-item
-            v-for="(tab, i) in tabs"
-            :key="i"
-            :to="localePath(tab.route)"
-          >
-            <v-list-item-title class="text-right">
-              {{ tab.name }}
-            </v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </div>
-    </v-tab-item>
-    <ProfilePanel v-model="settings" />
-  </v-tabs>
+  <div class="hero">
+    <HeroBanner
+      :hero-title="$t('hero-title')"
+      :hero-subtitle="$t('hero-subtitle')"
+      background-image="/img/portal/banner-hero.png"
+      logo="/img/logo-inteira-antiga-branca.svg"
+    />
+    <div class="body">
+      <v-row class="body--wrapper">
+        <v-col>
+          <h2>{{ $t('section1-title') }}</h2>
+          <p>
+            {{ $t('section1-paragraph') }} <span style="color:#F58A1F; font-weight: bold;">{{ $t('highlighted-text') }}</span>
+            {{ $t('section1-continuation') }} <br />
+            {{ $t('section1-additional-info') }}
+          </p>
+          <a class="link-primary" :href="localePath('/projeto')">{{ $t('learn-more') }}</a>
+        </v-col>
+        <a href="/">
+          <v-img max-width="300" :src="hero_content01" />
+        </a>
+      </v-row>
+      <v-row class="body--wrapper">
+        <v-img max-width="300" :src="hero_content02" />
+        <v-col>
+          <h2>{{ $t('section2-title') }}</h2>
+          <p>
+            {{ $t('section2-paragraph') }}
+          </p>
+          <a class="link-primary" :href="localePath('/como-funciona')">{{ $t('learn-more') }}</a>
+        </v-col>
+      </v-row>
+    </div>
+  </div>
 </template>
 
 <i18n>
 {
   "en": {
-    "analytics-tab": "Analytics",
-    "catalog-tab": "My Image Database",
-    "search-tab": "Monitoring",
-    "layers-tab": "Layers",
-    "high-resolution-mosaics-tab": "High Resolution and Mosaics",
-    "support-fire-tab": "Fire Hazard and Hot Spots (INPE)",
-    "landuse-tab": "Land Use And Occupation",
-    "prodes-tab": "PRODES (INPE)",
-    "urgent-alerts-tab": "Urgent Alerts",
-    "priority-tab": "Priorities",
-    "document-tab": "Document",
-    "mapoteca-tab": "Map Library",
-    "deter-tab": "Deter",
-    "logs-tab": "Graphics View"
+    "hero-title": "The protection of indigenous territories is an important tool for preserving natural resources and promoting respect for Brazil's cultural diversity.",
+    "hero-subtitle": "It is through the permanent possession and exclusive use of the lands they traditionally occupy that indigenous peoples develop their social organization, customs, languages, beliefs, and traditions. With the aim of guaranteeing these original rights, Funai conceived the Remote Monitoring Center (CMR), a project aimed at monitoring indigenous lands and preserving our history and natural resources.",
+    "section1-title": "Remote Monitoring Center",
+    "section1-paragraph": "Almost 13% of the national territory is made up of indigenous lands.",
+    "highlighted-text": "The Remote Monitoring Center (CMR)",
+    "section1-continuation": "was developed to enable daily monitoring of occurrences such as deforestation, degradation, land-use change, and occupation in these territories.",
+    "section1-additional-info": "The information generated from images captured by the Landsat-8 satellite provides essential support not only for territorial protection planning but also for indigenist work as a whole.",
+    "learn-more": "» learn more",
+    "section2-title": "How Does It Work?",
+    "section2-paragraph": "The Remote Monitoring Center is a platform established by Funai's General Coordination of Territorial Monitoring (CGMT, created by Decree No. 7,056 of December 28, 2009, and reaffirmed by Decree No. 7,778 of July 27, 2012). Its function is to integrate information that underpins the planning of indigenist actions. The database, which has been fed since 2015, provides data extracted from images produced cyclically, with a spatial resolution of 30 meters, where the smallest unit of the image represents 30 meters on the Earth's surface. The almost biweekly surface scan makes it possible to detect changes such as clear-cutting, degradation, deforestation in regeneration areas, and forest fires."
   },
   "pt-br": {
-    "analytics-tab": "Analítico",
-    "catalog-tab": "Meu acervo de imagens",
-    "search-tab": "Monitoramento Diário",
-    "layers-tab": "Camadas de Sobreposição",
-    "high-resolution-mosaics-tab": "Alta Resolução e Mosaicos",
-    "support-fire-tab": "Risco de Fogo e Focos de Calor (INPE)",
-    "landuse-tab": "Uso e Ocupação do Solo",
-    "prodes-tab": "PRODES (INPE)",
-    "urgent-alerts-tab": "Alerta Urgente",
-    "priority-tab": "Polígonos Prioritários",
-    "document-tab": "Documental",
-    "mapoteca-tab": "Mapoteca",
-    "deter-tab": "Deter",
-    "logs-tab": "Gráficos de Acesso"
+    "hero-title": "A proteção dos territórios indígenas é um importante instrumento de preservação dos recursos naturais e de promoção ao respeito à diversidade cultural do Brasil.",
+    "hero-subtitle": "É a partir da posse permanente e uso exclusivo das terras que tradicionalmente ocupam que os povos indígenas desenvolvem sua organização social, costumes, línguas, crenças e tradições. Pensando na garantia desses direitos originários, a Funai idealizou o Centro de Monitoramento Remoto (CMR), um projeto que tem por objetivo monitorar as terras indígenas e preservar a nossa história e recursos naturais.",
+    "section1-title": "Centro de Monitoramento Remoto",
+    "section1-paragraph": "Quase 13% do território nacional é composto por terras indígenas.",
+    "highlighted-text": "O Centro de Monitoramento Remoto (CMR)",
+    "section1-continuation": "foi desenvolvido para possibilitar o acompanhamento diário de ocorrências como desmatamento, degradação, mudança de uso e de ocupação do solo nesses territórios.",
+    "section1-additional-info": "As informações geradas a partir de imagens captadas pelo satélite Landsat-8 são subsídios indispensáveis não só ao planejamento de ações de proteção territorial, mas ao trabalho indigenista como um todo.",
+    "learn-more": "» saiba mais",
+    "section2-title": "Como Funciona?",
+    "section2-paragraph": "O Centro de Monitoramento Remoto é uma plataforma instituída pela Coordenação Geral de Monitoramento Territorial da Funai (CGMT, criada pelo Decreto nº 7.056, de 28 de dezembro de 2009, e ratificada pelo Decreto nº 7.778, de 27 de julho de 2012). Sua função é integrar informações que fundamentem o planejamento de ações indigenistas. O banco que vem sendo alimentado desde o ano de 2015 disponibiliza dados extraídos de imagens produzidas ciclicamente, com resolução espacial de 30 metros, em que a menor unidade da imagem representa 30 metros na superfície da Terra. A varredura praticamente quinzenal da superfície torna possível a detecção de mudanças como corte raso, degradação, desmatamento em área de regeneração e fogo em floresta."
   }
 }
 </i18n>
-
 <script>
-import { mapState, mapMutations } from 'vuex';
-import ProfilePanel from '@/components/profile/ProfilePanel';
+import HeroBanner from '@/components/panel/HeroBanner'
 
 export default {
-  name: 'MapLayersPanel',
-
-  components: { ProfilePanel },
-
+  name: 'Portal',
+  layout: 'portal',
+  components: {
+    HeroBanner
+  },
   data() {
     return {
-      logo_funai: process.env.DEFAULT_LOGO_IMAGE_CMR,
-      activeTab: 0,
-      compareTabIndex: null,
-      settings: false,
+      logo_cmr: '/img/logo-inteira-antiga-branca.svg',
+      hero_content01: '/img/portal/mapaaldeias.jpg',
+      hero_content02: '/img/portal/imglogoindigenous.jpg',
+      showProjectSubmenu: false,
+      showHowItWorksSubmenu: false,
+      projectSubmenus: [
+        { title: 'Conheça o Projeto', route: '/projeto' },
+        { title: 'Terras Indígenas', route: '/terras-indigenas' },
+      ],
+      howItWorksSubmenus: [
+        { title: 'Mais informações', route: '/como-funciona' },
+        { title: 'Connheça o CMR', route: '/video' },
+      ]
     };
-  },
-
-  computed: {
-    isIndex() {
-      return this.getRouteBaseName() === 'index';
-    },
-    allTabs() {
-      /*
-        TODO: Commented modules will be added post-launch cmr2
-
-        {
-          name: this.$t('priority-tab'),
-          icon: 'mdi-map-marker-alert',
-          route: '/priority',
-          show: process.env.ROUTE_PRIORITY === 'true',
-        },
-        {
-          name: this.$t('document-tab'),
-          icon: 'mdi-file-document',
-          route: '/document',
-          show: process.env.ROUTE_DOCUMENT === 'true',
-        },
-        {
-          name: this.$t('deter-tab'),
-          icon: 'mdi-leaf',
-          route: '/deter',
-          show: process.env.ROUTE_DETER === 'true',
-        },
-        {
-          name: this.$t('prodes-tab'),
-          icon: 'mdi-view-dashboard',
-          route: '/support-prodes',
-          show: process.env.ROUTE_SUPPORT_PRODES === 'true',
-        },
-           {
-          name: this.$t('catalog-tab'),
-          icon: 'mdi-folder-multiple-image',
-          route: '/catalog',
-          show: process.env.ROUTE_CATALOG === 'true',
-        },
-          {
-          name: this.$t('support-fire-tab'),
-          icon: 'mdi-fire',
-          route: '/support-hazard',
-          show: process.env.ROUTE_SUPPORT_HAZARD === 'true',
-        },
-        */
-
-      return [
-        {
-          name: this.$t('layers-tab'),
-          icon: 'mdi-layers',
-          route: '/support',
-          show: process.env.ROUTE_SUPPORT === 'true',
-        },
-
-        {
-          name: this.$t('search-tab'),
-          icon: 'mdi-map-search',
-          route: '/monitoring',
-          show: process.env.ROUTE_MONITORING === 'true',
-        },
-        {
-          name: this.$t('high-resolution-mosaics-tab'),
-          icon: 'mdi-book-open-page-variant',
-          route: '/support-raster',
-          show: process.env.ROUTE_SUPPORT_RASTER === 'true',
-        },
-        {
-          name: this.$t('landuse-tab'),
-          icon: 'mdi-sprout',
-          route: '/land-use',
-          show: process.env.ROUTE_LAND_USE === 'true',
-        },
-        {
-          name: this.$t('mapoteca-tab'),
-          icon: 'mdi-dresser',
-          route: '/mapoteca',
-          show: process.env.ROUTE_MAPOTECA === 'true',
-        },
-        {
-          name: this.$t('analytics-tab'),
-          icon: 'mdi-chart-box-outline',
-          route: '/statistics',
-          show: process.env.ROUTE_ANALYTICS === 'true',
-        },
-      ];
-    },
-    tabs() {
-      return this.allTabs.filter((tab) => tab.show);
-    },
-    ...mapState('catalog', ['isComparing']),
-    ...mapState('tableDialog', ['showTableDialog']),
-  },
-
-  watch: {
-    isComparing: 'handleCompareTab',
-  },
-
-  created() {
-    if (process.env.IMAGERY === 'false') {
-      this.tabs.splice(1, 1);
-    }
-    this.handleCompareTab();
-    if (this.$route.path === '/statistics') {
-      this.settings = true;
-    }
-  },
-
-  methods: {
-    handleCompareTab() {
-      if (this.isComparing) {
-        this.tabs.push({
-          name: this.$t('compare-tab'),
-          icon: 'mdi-compare',
-          route: '/catalog/comparator',
-        });
-        this.compareTabIndex = this.tabs.length - 1;
-        this.$router.push(this.localePath('/catalog/comparator'));
-      } else if (this.compareTabIndex !== null) {
-        this.tabs.splice(this.compareTabIndex, 1);
-        this.compareTabIndex = null;
-      }
-    },
-    closeRightDrawer() {
-      this.closeDrawer();
-    },
-    ...mapMutations('userProfile', ['closeDrawer']),
   },
 };
 </script>
 
-<style lang="sass">
-  .v-list-item__title
-    margin-bottom: 15px !important
+<style lang="sass" scoped>
+a
+  color: #FFCC00
+  font-weight: bold
+  text-decoration: none
 
-  .tab-icon
-    padding-top: 4px
-    color: white !important
-    font-size: 180% !important
+  &:hover
+    color: #d92b3f
 
-  .right-tabs
-    flex-direction: row-reverse
-    position: relative
-    min-height: 550px
+.v-menu__content
+  background-color: white
+  color: black
 
-    > .v-slide-group > .v-slide-group__wrapper > .v-slide-group__content
-      min-height: 550px
-      > .v-tab
-        min-width: 0px
+.menu-with-submenu
+  position: relative
 
-    > .v-tabs-slider-wrapper
-      left: 52px !important
-
-    > .v-window, > .v-window > .v-window__container > .v-window-item
-      overflow-y: auto
-      height: 100%
-      display: block !important
-      margin-bottom: 50px
-
-  .scroll-y-transition-enter-active,
-  .scroll-y-transition-leave-active
-    transition: 0.15s cubic-bezier(0.25, 0.8, 0.5, 1) !important
-
-  .back-mobile-button
-    min-width: 0 !important
-    display: block
-    border-radius: none
-    box-shadow: none
-    height: 64px !important
-    background-color: transparent !important
-    color: white !important
-
-  .v-dialog > .v-card > .v-card__title
-    background: #DA2A3F
-    color: whitesmoke
-    padding: 8px 16px
-
-    > .v-btn--icon
-      color: whitesmoke
-
-  #img-wrapper
+  .submenu
     position: absolute
-    bottom: 0
+    top: 110%
+    left: 0
+    background-color: white
+    width: 250px
+    border: 1px solid #ccc
+    border-radius: 8px
+    padding: 0.5rem
+    display: flex
+    flex-direction: column
+    border-top: 5px solid #F58A1F
+
+    .submenu-item
+      padding: 0.5rem 1rem
+      color: #333
+      text-decoration: none
+      transition: all easy-in-out 0.5
+
+      &:hover
+        background-color: #F58A1F
+        border-radius: 8px
+        color: white
+
+.hero
+  &--banner
+    height: 700px
+    background-image: url('/img/portal/banner-hero.png')
+    background-position: bottom
+
+  &--nav-bar
+    display: flex
+    gap: 2rem
+
+  &--content
+    max-width: 450px
+    margin: 2% 6%
+
+    h1
+      color: #FFCC00
+      line-height: 1.2
+      font-size: 22px
+      font-weight: 600
+
+    p
+      color: white
+      font-size: 14px
+      line-height: 22px
+      padding-top: 1rem
+
+  &--wrapper
+    width: 80%
+
+    &-row
+      width: 100%
+      margin: 0 2%
+
+  .body
+    justify-content: center
+    align-items: center
     width: 100%
-    height: 50px
-    background: #F5F5F5
-    padding: 5px 0
+    display: flex
+    flex-direction: column
 
-  @media (max-width: 768px)
-    .v-list-item__title
-      margin-bottom: 12px !important
-      font-size: 0.9rem !important
+    &--wrapper
+      max-width: 55%
+      align-items: center
+      justify-content: center
+      padding: 3rem 0
+      gap: 2rem
 
-    .tab-icon
-      padding-top: 4px
-      color: white !important
-      font-size: 150% !important
-
-  @media (max-height: 620px)
-    .v-list-item__title
-      margin-bottom: 0 !important
-
-    .v-list-item
-      min-height: 40px
-
-    .v-tabs--vertical > .v-tabs-bar .v-tab
-      height: 41px
 
 </style>
