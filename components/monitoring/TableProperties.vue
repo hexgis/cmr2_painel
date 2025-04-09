9679a88b295ff94df67a31fcab22eaf4dd7f56a2
Gabriel Freitas <gabrielmarianosf@gmail.com>
2025-04-09 18:31:06 -0300

    feat: Add download button and loading state for table monitoring

--- components/monitoring/TableProperties.vue
+++ components/monitoring/TableProperties.vue
@@ -1,91 +1,76 @@
 <template>
-  <v-dialog
-    v-model="tableDialogMonitoring"
-    width="80vw"
-    persistent
-    no-click-animation
-    :fullscreen="$vuetify.breakpoint.smAndDown"
-  >
-    <v-card>
-      <v-toolbar
-        dark
-        color="secondary"
-      >
-        <h3>{{ $t('table-label') }}</h3>
-        <v-spacer />
-        <v-btn
-          icon
-          @click="tableDialogMonitoring = false"
-        >
-          <v-icon>mdi-close</v-icon>
-        </v-btn>
-      </v-toolbar>
+    <v-dialog
+        v-model="tableDialogMonitoring"
+        width="80vw"
+        persistent
+        no-click-animation
+        :fullscreen="$vuetify.breakpoint.smAndDown"
+    >
+        <v-card>
+            <v-toolbar dark color="secondary">
+                <h3>{{ $t('table-label') }}</h3>
+                <v-spacer />
+                <v-btn icon @click="tableDialogMonitoring = false">
+                    <v-icon>mdi-close</v-icon>
+                </v-btn>
+            </v-toolbar>
 
-      <v-card-text class="mt-4">
-        <a class="d-flex justify-end">
-          <v-btn
-            small
-            fab
-            class="mx-2 my-2"
-            color="secondary"
-            :loading="false"
-            @click="downloadTableMonitoring"
-          >
-            <v-icon>mdi-download</v-icon>
-          </v-btn>
-        </a>
+            <v-card-text class="mt-4">
+                <v-row no-gutters>
+                  <v-spacer></v-spacer>
+                  <DialogConfirmDownload table />
 
-        <v-data-table
-          :headers="headers"
-          :items-per-page="tablePropertiesItemsPerPage"
-          :page="tablePropertiesPage"
-          :items="tableMonitoring"
-          :loading="loadingSearchTableMonitoring"
-          :disable-filtering="loadingSearchTableMonitoring"
-          :disable-pagination="loadingSearchTableMonitoring"
-          :disable-sort="loadingSearchTableMonitoring"
-          :server-items-length="tablePropertiesTotal"
-          :footer-props="{
-            itemsPerPageOptions: [5, 10, 50, tablePropertiesTotal],
-          }"
-          class="font-weight-regular"
-          multi-sort
-          fixed-header
-          mobile-breakpoint="0"
-          @update:options="onOptionsUpdate"
-        >
-          <!-- :height="$vuetify.breakpoint.smAndDown ? '' : '65vh'" -->
-          <template
-            v-if="[item.prioridade]"
-            #[`item.prioridade`]="{ item }"
-          >
-            <v-row>
-              <v-col>
-                <v-chip
-                  class="mt-2"
-                  :color="getColor(item.prioridade)"
-                  :dark="
-                    getColor(item.prioridade) !== 'yellow'
-                  "
+                </v-row>
+                
+
+                <v-data-table
+                    :headers="headers"
+                    :items-per-page="tablePropertiesItemsPerPage"
+                    :page="tablePropertiesPage"
+                    :items="tableMonitoring"
+                    :loading="loadingSearchTableMonitoring"
+                    :disable-filtering="loadingSearchTableMonitoring"
+                    :disable-pagination="loadingSearchTableMonitoring"
+                    :disable-sort="loadingSearchTableMonitoring"
+                    :server-items-length="tablePropertiesTotal"
+                    :footer-props="{
+                        itemsPerPageOptions: [5, 10, 50, tablePropertiesTotal],
+                    }"
+                    class="font-weight-regular"
+                    multi-sort
+                    fixed-header
+                    mobile-breakpoint="0"
+                    @update:options="onOptionsUpdate"
                 >
-                  {{ item.prioridade }}
-                </v-chip>
-              </v-col>
-            </v-row>
-          </template>
-          <template
-            v-if="[item.action]"
-            #[`item.actions`]="{ item }"
-          >
-            <MapPrinterPriority
-              class="mx-2 mb-2"
-              :value="dialogPrint"
-            />
-          </template>
-        </v-data-table>
-      </v-card-text>
-    </v-card>
-  </v-dialog>
+                    <!-- :height="$vuetify.breakpoint.smAndDown ? '' : '65vh'" -->
+                    <template
+                        v-if="[item.prioridade]"
+                        #[`item.prioridade`]="{ item }"
+                    >
+                        <v-row>
+                            <v-col>
+                                <v-chip
+                                    class="mt-2"
+                                    :color="getColor(item.prioridade)"
+                                    :dark="
+                                        getColor(item.prioridade) !== 'yellow'
+                                    "
+                                >
+                                    {{ item.prioridade }}
+                                </v-chip>
+                            </v-col>
+                        </v-row>
+                    </template>
+                    <template v-if="[item.action]" #[`item.actions`]="{ item }">
+                        <MapPrinterPriority
+                            class="mx-2 mb-2"
+                            :value="dialogPrint"
+                        />
+                    </template>
+                </v-data-table>
+            </v-card-text>
+        </v-card>
+    </v-dialog>
 </template>
 
 <i18n>
@@ -100,134 +85,138 @@
 </i18n>
 
 <script>
-import { mapState, mapMutations, mapActions } from 'vuex';
+import { mapState, mapMutations, mapActions } from 'vuex'
+import DialogConfirmDownload from './DialogConfirmDownload.vue'
 
 export default {
-  name: 'TablePropertiesMonitoring',
+    name: 'TablePropertiesMonitoring',
 
-  components: {},
+    components: { DialogConfirmDownload },
 
-  //   props: {
-  //     table: {
-  //       type: Boolean,
-  //       require: true,
-  //     },
-  //   },
+    //   props: {
+    //     table: {
+    //       type: Boolean,
+    //       require: true,
+    //     },
+    //   },
 
-  data() {
-    return {
-      headers: [
-        { text: 'Código Funai', value: 'co_funai' },
-        { text: 'Terra Indígena', value: 'no_ti' },
-        { text: 'Coordenação Regional', value: 'ds_cr' },
-        { text: 'Classe', value: 'no_estagio' },
-        { text: 'Data da Imagem', value: 'dt_imagem' },
-        { text: 'Área do Polígono (ha)', value: 'nu_area_ha' },
-        { text: 'Latitude', value: 'nu_latitude' },
-        { text: 'Longitude', value: 'nu_longitude' },
-      ],
-    };
-  },
-
-  computed: {
-    tableDialogMonitoring: {
-      get() {
-        return this.$store.state.tableDialog.showTableDialog;
-      },
-
-      set(value) {
-        this.$store.commit('tableDialog/setshowTableDialog', value);
-      },
+    data() {
+        return {
+            headers: [
+                { text: 'Código Funai', value: 'co_funai' },
+                { text: 'Terra Indígena', value: 'no_ti' },
+                { text: 'Coordenação Regional', value: 'ds_cr' },
+                { text: 'Classe', value: 'no_estagio' },
+                { text: 'Data da Imagem', value: 'dt_imagem' },
+                { text: 'Área do Polígono (ha)', value: 'nu_area_ha' },
+                { text: 'Latitude', value: 'nu_latitude' },
+                { text: 'Longitude', value: 'nu_longitude' },
+            ],
+        }
     },
 
-    tablePropertiesTotal() {
-      return this.$store.state.monitoring.tableMonitoringTableOptions
-        .totalItems || 5;
+    computed: {
+        tableDialogMonitoring: {
+            get() {
+                return this.$store.state.tableDialog.showTableDialog
+            },
+
+            set(value) {
+                this.$store.commit('tableDialog/setshowTableDialog', value)
+            },
+        },
+
+        tablePropertiesTotal() {
+            return (
+                this.$store.state.monitoring.tableMonitoringTableOptions
+                    .totalItems || 5
+            )
+        },
+
+        tablePropertiesPage: {
+            get() {
+                return this.$store.state.monitoring.tableMonitoringTableOptions
+                    .page
+            },
+
+            set(value) {
+                this.$store.commit('monitoring/setTablePropertiesPage', value)
+            },
+        },
+
+        tablePropertiesItemsPerPage: {
+            get() {
+                return this.$store.state.monitoring.tableMonitoringTableOptions
+                    .itemsPerPage
+            },
+
+            set(value) {
+                this.$store.commit(
+                    'monitoring/setTablePropertiesItemsPerPage',
+                    value
+                )
+            },
+        },
+
+        ...mapState('monitoring', [
+            'showTableDialogMonitoring',
+            'loadingSearchTableMonitoring',
+            'tableMonitoring',
+            'tableMonitoringTableOptions',
+        ]),
+        ...mapState('tableDialog', ['showTableDialog']),
     },
 
-    tablePropertiesPage: {
-      get() {
-        return this.$store.state.monitoring.tableMonitoringTableOptions
-          .page;
-      },
+    methods: {
+        getColor(prioridade) {
+            switch (prioridade) {
+                case 'Muito Alta':
+                    return '#9400D3'
+                    break
+                case 'Alta':
+                    return 'red'
+                    break
+                case 'Media':
+                    return 'orange'
+                    break
+                case 'Baixa':
+                    return 'yellow'
+                    break
+                case 'Muito Baixa':
+                    return 'green'
+                    break
+                default:
+                    break
+            }
+        },
 
-      set(value) {
-        this.$store.commit('monitoring/setTablePropertiesPage', value);
-      },
+        onOptionsUpdate(newOptions) {
+            const currentOptions = this.tableMonitoringTableOptions
+
+            const hasChanged =
+                newOptions.itemsPerPage !== currentOptions.itemsPerPage ||
+                newOptions.page !== currentOptions.page
+
+            if (!hasChanged) return
+
+            this.setTableMonitoringTableOptions({
+                itemsPerPage: newOptions.itemsPerPage,
+                page: newOptions.page,
+            })
+
+            this.$store.dispatch('monitoring/getPropertiesTableMonitoring')
+        },
+
+        ...mapMutations('monitoring', [
+            'setShowTableDialogMonitoring',
+            'getPropertiesTableMonitoring',
+            'setTableMonitoringTableOptions',
+        ]),
+        ...mapActions('monitoring', ['downloadTableMonitoring']),
     },
 
-    tablePropertiesItemsPerPage: {
-      get() {
-        return this.$store.state.monitoring.tableMonitoringTableOptions
-          .itemsPerPage;
-      },
-
-      set(value) {
-        this.$store.commit(
-          'monitoring/setTablePropertiesItemsPerPage',
-          value,
-        );
-      },
+    mounted() {
+        this.$store.dispatch('monitoring/getPropertiesTableMonitoring')
     },
-
-    ...mapState('monitoring', [
-      'showTableDialogMonitoring',
-      'loadingSearchTableMonitoring',
-      'tableMonitoring',
-      'tableMonitoringTableOptions',
-    ]),
-    ...mapState('tableDialog', ['showTableDialog']),
-  },
-
-  methods: {
-    getColor(prioridade) {
-      switch (prioridade) {
-        case 'Muito Alta':
-          return '#9400D3';
-          break;
-        case 'Alta':
-          return 'red';
-          break;
-        case 'Media':
-          return 'orange';
-          break;
-        case 'Baixa':
-          return 'yellow';
-          break;
-        case 'Muito Baixa':
-          return 'green';
-          break;
-        default:
-          break;
-      }
-    },
-
-    onOptionsUpdate(newOptions) {
-      const currentOptions = this.tableMonitoringTableOptions;
-
-      const hasChanged = newOptions.itemsPerPage !== currentOptions.itemsPerPage
-                || newOptions.page !== currentOptions.page;
-
-      if (!hasChanged) return;
-
-      this.setTableMonitoringTableOptions({
-        itemsPerPage: newOptions.itemsPerPage,
-        page: newOptions.page,
-      });
-
-      this.$store.dispatch('monitoring/getPropertiesTableMonitoring');
-    },
-
-    ...mapMutations('monitoring', [
-      'setShowTableDialogMonitoring',
-      'getPropertiesTableMonitoring',
-      'setTableMonitoringTableOptions',
-    ]),
-    ...mapActions('monitoring', ['downloadTableMonitoring']),
-  },
-
-  mounted() {
-    this.$store.dispatch('monitoring/getPropertiesTableMonitoring');
-  },
-};
+}
 </script>
--- store/monitoring.js
+++ store/monitoring.js
@@ -46,6 +46,7 @@ export const state = () => ({
   isLoadingGeoJson: false,
   isLoadingFeatures: false,
   isLoadingTableMonitoring: false,
+  isLoadingDownloadTableMonitoring: false,
   filterOptions: {
     regionalFilters: [],
     tiFilters: [],
@@ -91,6 +92,10 @@ export const getters = {
 export const mutations = {
   setShowTableDialogMonitoring(state, showTableDialogMonitoring) {
     state.showTableDialogMonitoring = showTableDialogMonitoring;
+  },
+
+  setIsLoadingDownloadTableMonitoring(state, isLoadingDownloadTableMonitoring) {
+    state.isLoadingDownloadTableMonitoring = isLoadingDownloadTableMonitoring;
   },
 
   setLoadingSearchTableMonitoring(state, loadingSearchTableMonitoring) {
@@ -1310,7 +1315,78 @@ export const actions = {
 
   async downloadTableMonitoring({ commit, state }) {
     try {
-      const items = state.tableMonitoring;
+      commit('setIsLoadingDownloadTableMonitoring', true);
+      const params = {
+        service: 'WFS',
+        version: '1.0.0',
+        request: 'GetFeature',
+        typeName: state.geoserverLayerMonitoring,
+        outputFormat: 'application/json',
+        CQL_FILTER: '',
+        maxFeatures: 10000,
+      };
+
+      let url;
+      url = state.urlWmsMonitoring;
+
+      // gera os parametros
+      if (state.intersectsWmsMonitoring) {
+        params.CQL_FILTER += state.intersectsWmsMonitoring;
+      }
+
+
+      const arrayTI = [];
+      if (state.filters.ti && state.filters.ti.length) {
+        Object.values(state.filters.ti).forEach((item) => {
+          arrayTI.push(item.co_funai);
+        });
+        if (params.CQL_FILTER.length) {
+          params.CQL_FILTER += ' AND ';
+        }
+        params.CQL_FILTER += `co_funai IN (${arrayTI.toString()})`;
+      }
+
+      const arrayCR = [];
+      if (state.filters.cr && state.filters.cr.length) {
+        Object.values(state.filters.cr).forEach((item) => {
+          arrayCR.push(item.co_cr);
+        });
+        if (params.CQL_FILTER.length) {
+          params.CQL_FILTER += ' AND ';
+        }
+        params.CQL_FILTER += `co_cr IN (${arrayCR.toString()})`;
+      }
+
+      if (state.filters.startDate && state.filters.endDate) {
+        if (params.CQL_FILTER.length) {
+          params.CQL_FILTER += ' AND ';
+        }
+        params.CQL_FILTER += `(dt_t_um >= (${state.filters.startDate}) AND dt_t_um <= (${state.filters.endDate}))`;
+      }
+
+      const filtersSubLayersTrue = Object.keys(state.monitoringSubLayers).filter(key => state.monitoringSubLayers[key] === true);
+      if (filtersSubLayersTrue && filtersSubLayersTrue.length) {
+        params.CQL_FILTER += ' AND';
+        let sublayers = '';
+        filtersSubLayersTrue.forEach((value, key) => {
+          if (!state.monitoringSubLayers[value]) {
+            return;
+          }
+          if (key === 0) {
+            sublayers += ` no_estagio = '${value}'`;
+          }
+          if (key > 0) {
+            sublayers += ` OR no_estagio = '${value}'`;
+          }
+        });
+        params.CQL_FILTER += `(${sublayers})`;
+      }
+
+      const paramsUrl = new URLSearchParams(params);
+      url = `${url}${paramsUrl}`;
+
+      const response = await this.$api.$get(url, { params });
+      const items = response.features.map((feature) => feature.properties);
 
       if (!items.length) return;
 
@@ -1348,6 +1424,8 @@ export const actions = {
         },
         { root: true },
       );
+    } finally {
+      commit('setIsLoadingDownloadTableMonitoring', false);
     }
   },
 
