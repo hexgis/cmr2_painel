6df7a6ee6c7c196e443e733c08b2b8db8264e75f
ricardo <ricardo.nunes@hex360.com.br>
2025-06-18 12:47:24 -0300

    feat(cmr2): :sparkles: Add raster layer components and functionality

---
+++ components/raster/LayersRaster.vue
@@ -0,0 +1,33 @@
+<template>
+  <l-layer-group
+    name="supportLayersRaster"
+    :visible="showFeaturesSupportLayersRaster"
+  >
+    <template v-for="layer in supportLayersCategoryRaster">
+      <SupportLayerItem
+        :key="layer.id"
+        :layer="layer"
+      />
+    </template>
+  </l-layer-group>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+import SupportLayerItem from './SupportLayerItem';
+
+export default {
+  name: 'LayersRaster',
+
+  components: {
+    SupportLayerItem,
+  },
+
+  computed: mapState('supportLayers', [
+    'supportLayers',
+    'showFeaturesSupportLayersRaster',
+    'supportLayersCategoryRaster',
+  ]),
+};
+</script>
