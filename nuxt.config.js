8228063261437cd57adc3146aabf654148519ebf
joao.fonseca <joao.fonseca@hex360.com.br>
2025-09-23 20:00:41 -0300

    feat: :sparkles: add CSV plugin to nuxt configuration for enhanced data handling

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,6 +45,7 @@ export default {
     '~/plugins/filters.js',
     '~/plugins/v-mask.js',
     '~/plugins/i18n.js',
+    '~/plugins/csv.js',
     { src: '~/plugins/theme-config.js', mode: 'client' },
     { src: '~/plugins/vue-chartjs.js', mode: 'client' },
     { src: '~/plugins/vue2-leaflet-markercluster.js', mode: 'client' },
